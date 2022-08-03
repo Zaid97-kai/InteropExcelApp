4f6a9ca53e83633c1a6ba9d91a535b55ec5d4fdb
Zaid <ZZMingaliev@kai.ru>
2022-08-04 01:11:12 +0300

    Начата реализация экспорта в Word

--- InteropExcelApp/MainWindow.xaml.cs
+++ InteropExcelApp/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace InteropExcelApp
 {
@@ -135,5 +136,64 @@ namespace InteropExcelApp
 
             }
         }
+
+        private void BnExportWord_Click(object sender, RoutedEventArgs e)
+        {
+            List<Student> allStudents;
+            List<Group> allGroups;
+            using (UsersEntities usersEntities = new UsersEntities())
+            {
+                allStudents = usersEntities.Students.ToList().OrderBy(s => s.Name).ToList();
+                allGroups = usersEntities.Groups.ToList().OrderBy(g => g.NumberGroup).ToList();
+
+                var studentsCategories = allStudents.GroupBy(s => s.NumberGroupId).ToList();
+
+                var app = new Word.Application();
+                Word.Document document = app.Documents.Add();
+
+                foreach (var group in allGroups)
+                {
+                    Word.Paragraph paragraph = document.Paragraphs.Add();
+                    Word.Range range = paragraph.Range;
+
+                    range.Text = Convert.ToString(group.NumberGroup);
+                    paragraph.set_Style("Title");
+                    range.InsertParagraphAfter();
+
+                    Word.Paragraph tableParagraph = document.Paragraphs.Add();
+                    Word.Range tableRange = tableParagraph.Range;
+                    Word.Table studentsTable = document.Tables.Add(tableRange, allGroups.Count() + 1, 3);
+                    studentsTable.Borders.InsideLineStyle = studentsTable.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                    studentsTable.Range.Cells.VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+                    Word.Range cellRange;
+                    cellRange = studentsTable.Cell(1, 1).Range;
+                    cellRange.Text = "Порядковый номер";
+                    cellRange = studentsTable.Cell(1, 2).Range;
+                    cellRange.Text = "ФИО";
+                    studentsTable.Rows[1].Range.Bold = 1;
+                    studentsTable.Rows[1].Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                    for (int i = 0; i < allStudents.Count(); i++)
+                    {
+                        var currentStudent = allStudents[i];
+                        if (currentStudent.Group.NumberGroup == group.NumberGroup)
+                        {
+                            cellRange = studentsTable.Cell(i + 2, 1).Range;
+                            cellRange.Text = currentStudent.Id.ToString();
+
+                            cellRange = studentsTable.Cell(i + 2, 2).Range;
+                            cellRange.Text = currentStudent.Name;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+                app.Visible = true;
+            }
+        }
     }
 }
+
